4da3cec735237a654cc30246034d6eb871101b5d
Admantix <williamwijaya499@gmail.com>
2024-12-17 00:55:52 +0700

    Add utility functions, theme tokens, and UI components for enhanced styling and theming

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,99 @@
+"use client";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { ModeToggle } from "./ui/mode-toggle";
+
+import Gems from "@/public/assets/Gems.png";
+import Image from "next/image";
+import {
+  ChartBarIcon,
+  History,
+  Home,
+  SaveIcon,
+  ShoppingCart,
+} from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const items = [
+  {
+    title: "Dashboard",
+    icon: Home,
+    url: "/",
+  },
+  {
+    title: "Price Prediction",
+    icon: ChartBarIcon,
+    url: "/price-prediction",
+  },
+  {
+    title: "Marketplace",
+    icon: ShoppingCart,
+    url: "/marketplace",
+  },
+  {
+    title: "Wishlist",
+    icon: SaveIcon,
+    url: "/wishlist",
+  },
+  {
+    title: "Transactions",
+    icon: History,
+    url: "/transactions",
+  },
+];
+
+export function AppSidebar() {
+  const pathname = usePathname();
+  const isActive = (url: string) => pathname === url;
+
+  return (
+    <Sidebar collapsible="none" className="flex flex-col fixed border-r-2">
+      <SidebarHeader />
+      <SidebarGroupLabel>
+        <Image src={Gems} alt="" width={30} className="mx-1" />
+        <div className="text-2xl font-bold">Aurora Gems</div>
+        <div className="ml-auto">
+          <ModeToggle />
+        </div>
+      </SidebarGroupLabel>
+      <div className="mt-5 mx-3 flex-grow h-auto">
+        <SidebarGroupContent>
+          <SidebarMenu>
+            {items.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <Link href={item.url}>
+                  <div
+                    className={`flex items-center px-4 py-2 rounded-md transition-colors duration-200 ${
+                      isActive(item.url)
+                        ? "bg-purple-500 text-white"
+                        : "bg-transparent text-gray-700 hover:bg-gray-200"
+                    }`}
+                  >
+                    <item.icon className="mr-3" />
+                    <span className="font-semibold text-sm">{item.title}</span>
+                  </div>
+                </Link>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </div>
+      <SidebarContent>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
---
+++ themes/tokens.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  colors: {
+    primary: "#DFF2EB",
+    secondary: "#B9E5E8",
+    third: "#7AB2D3",
+    fourth: "#4A628A",
+    fifth: "#001F3F",
+  },
+};
