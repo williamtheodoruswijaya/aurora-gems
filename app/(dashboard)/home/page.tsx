c01652931026b6d53de267289066d827d741335c
Admantix <williamwijaya499@gmail.com>
2024-12-18 00:21:31 +0700

    Add server session handling to Home page and configure NextAuth secret

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div>Home Page</div>;
-}
+"use client";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+
+const Home = async () => {
+  const session = getServerSession(authOptions);
+  console.log(session);
+  return <div>Home</div>;
+};
+
+export default Home;
