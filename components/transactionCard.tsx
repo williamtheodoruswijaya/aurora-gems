b7d54580845741d2476e1d35d804aad64ff8f872
Admantix <williamwijaya499@gmail.com>
2024-12-19 20:49:33 +0700

    feat: implement transaction fetching and display in Transactions component

--- components/transactionCard.tsx
+++ components/transactionCard.tsx
@@ -1,54 +1,37 @@
-export interface Transaction {
-    id: number;
-    buyer: string;
-    date: string;
-    diamondType: string;
-    carat: number;
-    price: string;
-    status: 'Delivered' | 'Pending' | 'Shipped';
-  }
-  
-  const TransactionCard: React.FC<Transaction> = ({
-    buyer,
-    date,
-    diamondType,
-    carat,
-    price,
-    status,
-  }) => {
-    // Conditional styling for status
-    const statusColor =
-      status === 'Delivered'
-        ? 'bg-green-500'
-        : status === 'Pending'
-        ? 'bg-yellow-500'
-        : 'bg-blue-500';
-  
-    return (
-      <div className="bg-white border border-gray-300 text-black p-4 rounded-lg shadow-md flex justify-between items-center">
-        {/* Left Section */}
-        <div>
-          <h3 className="text-lg font-semibold">{buyer}</h3>
-          <p className="text-sm text-gray-400">{date}</p>
-          <p className="text-sm">
-            <span className="font-bold">Diamond:</span> {diamondType} ({carat} carat)
-          </p>
-          <p className="text-sm">
-            <span className="font-bold">Price:</span> {price}
-          </p>
-        </div>
-  
-        {/* Right Section */}
-        <div>
-          <span
-            className={`inline-block px-3 py-1 rounded-full text-xs font-medium ${statusColor}`}
-          >
-            {status}
-          </span>
+"use client";
+import TransactionCard from "@/components/transactionCard";
+import { Transaction } from "@/types/types";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  useEffect(() => {
+    const fetchTransaction = async () => {
+      try {
+        const response = await axios.get("/api/transactions");
+        setTransactions(response.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchTransaction();
+  }, []);
+
+  return (
+    <div className="flex h-screen text-white ml-64 mt-16 w-screen">
+      <div className="flex-1 flex flex-col">
+        <div className="p-6 space-y-4 overflow-y-auto w-full">
+          {transactions.length === 0 && (
+            <div className="text-lg font-semibold text-gray-600">
+              No Transaction yet.
+            </div>
+          )}
+          {transactions.map((transaction) => (
+            <TransactionCard key={transaction.id} {...transaction} />
+          ))}
         </div>
       </div>
-    );
-  };
-  
-  export default TransactionCard;
-  
+    </div>
+  );
+}
