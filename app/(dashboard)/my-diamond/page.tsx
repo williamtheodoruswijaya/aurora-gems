b23db2159d9b352f3aa330cd408bba65f000ac2a
Admantix <williamwijaya499@gmail.com>
2024-12-19 23:46:57 +0700

    feat: enhance diamond addition with user authentication and improved form handling

--- app/(dashboard)/my-diamond/page.tsx
+++ app/(dashboard)/my-diamond/page.tsx
@@ -6,29 +6,28 @@ import AddModal from "@/components/addmodal";
 import { Diamond } from "@/types/types";
 import axios from "axios";
 import { useToast } from "@/hooks/use-toast";
-import { z } from "zod";
+import * as z from "zod";
+import { useSession } from "next-auth/react";
 
 const diamondSchema = z.object({
   name: z.string().min(1, "Name is required").max(100),
   type: z.string().min(1, "Type is required").max(100),
   weight: z.number().min(1, "Weight is required"),
   price: z.number().min(1, "Price is required"),
+  listedById: z.number().int("ListedById must be an integer"),
 });
 
 export default function MyDiamonds() {
   const { toast } = useToast();
   const [selectedItem, setSelectedItem] = useState<Diamond | null>(null);
-  const [MyDiamondsItems, setMyDiamondsItems] = useState<Diamond[]>([]);
+  const [myDiamondsItems, setMyDiamondsItems] = useState<Diamond[]>([]);
   const [showModal, setShowModal] = useState(false);
   const [showAddModal, setShowAddModal] = useState(false);
-
-  // Form State
-  const [formData, setFormData] = useState({
-    name: "",
-    type: "",
-    weight: "",
-    price: "",
-  });
+  const [newDiamondName, setNewDiamondName] = useState("");
+  const [newDiamondType, setNewDiamondType] = useState("");
+  const [newDiamondWeight, setNewDiamondWeight] = useState("");
+  const [newDiamondPrice, setNewDiamondPrice] = useState("");
+  const { data: session, status } = useSession();
 
   useEffect(() => {
     const fetchDiamonds = async () => {
@@ -53,20 +52,28 @@ export default function MyDiamonds() {
     setShowAddModal(false);
   };
 
-  const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
   const handleFormSubmit = async (values: z.infer<typeof diamondSchema>) => {
     try {
+      if (isNaN(values.weight) || isNaN(values.price)) {
+        return toast({
+          title: "Weight and Price must be a number",
+          variant: "destructive",
+        });
+      }
+
+      if (status !== "authenticated") {
+        return toast({
+          title: "You must be logged in to add a diamond",
+          variant: "destructive",
+        });
+      }
+
       const parsedData = diamondSchema.parse({
         name: values.name,
         type: values.type,
-        weight: values.weight,
-        price: values.price,
+        weight: Number(values.weight),
+        price: Number(values.price),
+        listedById: values.listedById,
       });
 
       const response = await axios.post("/api/diamonds/create", {
@@ -74,37 +81,42 @@ export default function MyDiamonds() {
         type: parsedData.type,
         weight: parsedData.weight,
         price: parsedData.price,
+        listedById: parsedData.listedById,
       });
-      console.log(response);
-      setMyDiamondsItems((prev) => [...prev, response.data.data]);
 
-      setFormData({
-        name: "",
-        type: "",
-        weight: "",
-        price: "",
-      });
-  
       if (response.status === 200) {
         toast({
-          title: "Diamond purchased successfully",
-          description: "Thank you for shopping with us!",
+          title: "Diamond added successfully",
           variant: "default",
         });
+        setShowAddModal(false);
       }
-      setShowAddModal(false);
     } catch (error) {
-      console.error("Error adding diamond:", error);
+      console.error(error);
+      toast({
+        title: "Something went wrong",
+        variant: "destructive",
+      });
     }
   };
 
   return (
-    <div className="ml-64 overflow-y-auto p-8 min-h-screen">
+    <div className="ml-64 overflow-y-auto p-8 min-h-screen w-screen">
       {/* Title Section */}
       <div className="mb-6 mt-12">
-        {MyDiamondsItems.length === 0 ? (
-          <div className="text-center text-lg font-semibold text-gray-600">
-            You don’t own any diamonds...
+        {myDiamondsItems.length === 0 ? (
+          <div className="flex flex-row items-center justify-between">
+            <div className="text-center text-lg font-semibold text-gray-600">
+              You don’t own any diamonds...
+            </div>
+            <div className="ml-auto">
+              <button
+                className="bg-purple-600 text-white px-4 py-2 rounded-lg"
+                onClick={() => setShowAddModal(true)}
+              >
+                Add Diamond
+              </button>
+            </div>
           </div>
         ) : (
           <div className="flex flex-row items-center justify-between">
@@ -125,7 +137,7 @@ export default function MyDiamonds() {
       </div>
       {/* Diamonds Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-        {MyDiamondsItems.map((item) => (
+        {myDiamondsItems.map((item) => (
           <Card
             key={item.id}
             name={item.name}
@@ -155,48 +167,52 @@ export default function MyDiamonds() {
       {/* Add Diamond Modal */}
       {showAddModal && (
         <AddModal onClose={handleCloseModal}>
-          <h2 className="text-2xl font-bold mb-4 text-black">Add a New Diamond</h2>
+          <h2 className="text-2xl font-bold mb-4 text-black">
+            Add a New Diamond
+          </h2>
           <form className="space-y-4">
             <div>
-              <label className="block font-medium text-gray-700">Name</label>
+              <div className="block font-medium text-gray-700">Name</div>
               <input
                 type="text"
                 name="name"
-                value={formData.name}
-                onChange={handleFormChange}
+                value={newDiamondName}
+                onChange={(e) => setNewDiamondName(e.target.value)}
                 className="w-full p-2 border rounded bg-white text-black"
                 required
               />
             </div>
             <div>
-              <label className="block font-medium text-gray-700">Type</label>
+              <div className="block font-medium text-gray-700">Type</div>
               <input
                 type="text"
                 name="type"
-                value={formData.type}
-                onChange={handleFormChange}
+                value={newDiamondType}
+                onChange={(e) => setNewDiamondType(e.target.value)}
                 className="w-full p-2 border rounded bg-white text-black"
                 required
               />
             </div>
             <div>
-              <label className="block font-medium text-gray-700">Weight (gram)</label>
+              <div className="block font-medium text-gray-700">
+                Weight (gram)
+              </div>
               <input
                 type="number"
                 name="weight"
-                value={formData.weight}
-                onChange={handleFormChange}
+                value={newDiamondWeight}
+                onChange={(e) => setNewDiamondWeight(e.target.value)}
                 className="w-full p-2 border rounded bg-white text-black"
                 required
               />
             </div>
             <div>
-              <label className="block font-medium text-gray-700">Price ($)</label>
+              <div className="block font-medium text-gray-700">Price ($)</div>
               <input
                 type="number"
                 name="price"
-                value={formData.price}
-                onChange={handleFormChange}
+                value={newDiamondPrice}
+                onChange={(e) => setNewDiamondPrice(e.target.value)}
                 className="w-full p-2 border rounded bg-white text-black"
                 required
               />
@@ -209,18 +225,23 @@ export default function MyDiamonds() {
               >
                 Cancel
               </button>
-              <button
-                type="submit"
-                className="px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-700"
-                onSubmit={()=>handleFormSubmit({
-                  name: formData.name,
-                  type: formData.type,
-                  weight: Number(formData.weight),
-                  price: Number(formData.price),
-                })}
-              >
-                Add
-              </button>
+              {session && session.user && session.user.id ? (
+                <button
+                  type="submit"
+                  className="px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-700"
+                  onClick={() =>
+                    handleFormSubmit({
+                      name: newDiamondName,
+                      type: newDiamondType,
+                      weight: parseFloat(newDiamondWeight),
+                      price: parseInt(newDiamondPrice, 10),
+                      listedById: parseInt(session.user.id, 10),
+                    })
+                  }
+                >
+                  Add
+                </button>
+              ) : null}
             </div>
           </form>
         </AddModal>
